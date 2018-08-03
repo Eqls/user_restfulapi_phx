3aea6863d2b4ca66966c15c6bb867b5fe953163c
benonymus <bence1204@gmail.com>
2018-08-03 17:35:50 +0700

    a

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,8 @@ config :user_restfulapi_phx, UserRestfulapiPhxWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [],
+  server: true
 
 # ## SSL Support
 #
